6ffc154a6a93a9194b4a67e94cae0d9a60be86bf
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2020-09-18 11:15:34 +0200

    initial commit

---
+++ main.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+require 'csv'
+
+# Find data source
+# Open data source
+# Read it line by line
+# Refactor with wanted format "iramuteq"
+
+
+
+if ARGV.empty?
+  puts "No filename provided, please enter file name : "
+  file_name = "open_data.csv"
+  #file_name = gets
+else
+  file_name = ARGV.first
+end
+
+
+puts "File '#{file_name}' does not exists" unless File.exist? file_name
+puts "File '#{file_name}' seems to be empty" if File.zero? file_name
+data = File.read file_name
+
+csv_data = CSV.new(data, col_sep: ";")
+
+puts csv_data.readlines
+
+
+puts '> EOF'
